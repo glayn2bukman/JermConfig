005ad4e024fb2dafbe6ea3a39a93bbe24f3e4398
glayn2bukman <glayn2bukman@gmail.com>
2018-07-19 18:04:16 +0300

    fixed import error for Py3
    when installed as module, Py3 import was raising error

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
-from jcparser import JCParser, test
+import sys
+if sys.version_info[0]<3:
+    from jcparser import JCParser, test
+else:
+    from . import jcparser
+    JCParser = jcparser.JCParser
+    test = jcparser.test
